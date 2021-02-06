e234c72ed420fbae83dcac4855b99dd2ce8c0221
Jannis Gumz <jannis.gumz@mailbox.tu-dresden.de>
2021-02-06 08:24:52 +0100

    Implement move validity check for knight class

--- src/Knight.java
+++ src/Knight.java
@@ -12,6 +12,20 @@ public class Knight extends ChessPiece{
 
     @Override
     public boolean isValidMove(Tile target, Board board) {
-        return false;
+        // Moves in a L Shape -> 8 possible targets: x+1/y+2, x+1/y-2, x+2/y-1, x+2/y+1, x-1/y+2, x-1/y-2, x-2/y-1, x-2/y+1
+
+        int currentCol = this.getTile().getCol();
+        int currentRow = this.getTile().getRow();
+        int targetCol = target.getCol();
+        int targetRow = target.getRow();
+
+        return (currentRow + 1 == targetRow && currentCol + 2 == targetCol) ||
+                (currentRow + 1 == targetRow && currentCol - 2 == targetCol) ||
+                (currentRow - 1 == targetRow && currentCol + 2 == targetCol) ||
+                (currentRow - 1 == targetRow && currentCol - 2 == targetCol) ||
+                (currentRow + 2 == targetRow && currentCol + 1 == targetCol) ||
+                (currentRow + 2 == targetRow && currentCol - 1 == targetCol) ||
+                (currentRow - 2 == targetRow && currentCol + 1 == targetCol) ||
+                (currentRow - 2 == targetRow && currentCol - 1 == targetCol);
     }
 }
